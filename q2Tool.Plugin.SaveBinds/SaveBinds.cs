007cb937384471cae4161f1352c9057bfca450d9
Joao Vitor P. Moraes <jvlppm@gmail.com>
2010-01-09 18:01:43 -0200

    Identificado variaveis de binds

--- q2Tool.Plugin.SaveBinds/SaveBinds.cs
+++ q2Tool.Plugin.SaveBinds/SaveBinds.cs
@@ -23,7 +23,9 @@ namespace q2Tool
 
 		void aq2_OnPlayerChangeName(Action sender, PlayerChangeNameEventArgs e)
 		{
-			using (StreamWriter nickChanges = File.AppendText(Quake.Directory + "Action/Binds/nickChanges.txt"))
+			string filePath = Quake.Directory + "Action/Binds/nickChanges.txt";
+
+			using (StreamWriter nickChanges = File.Exists(filePath) ? File.AppendText(filePath) : new StreamWriter(filePath))
 				nickChanges.WriteLine("{0} -> {1}", e.OldName, e.Player.Name);
 		}
 
@@ -32,15 +34,9 @@ namespace q2Tool
 			if (!_binds.ContainsKey(nick))
 				LoadPlayerBinds(nick);
 
-			foreach (Player player in GetPlugin<PAction>().Players)
-				message = message.Replace(player.Name, "%K");
-
-			while (message.Contains("%K, %K"))
-				message = message.Replace("%K, %K", "%K");
-
-			while (message.Contains("%K and %K"))
-				message = message.Replace("%K and %K", "%K");
-
+			message = IdentifyPlayers(message);
+			message = IdentifyHitLocations(message);
+			message = IdentifyWeapons(message);
 
 			if(_binds[nick].ContainsKey(message))
 				_binds[nick][message]++;
@@ -48,13 +44,50 @@ namespace q2Tool
 				_binds[nick].Add(message, 1);
 		}
 
+		string IdentifyHitLocations(string message)
+		{
+			message = message.Replace("head", "%D");
+			message = message.Replace("stomach", "%D");
+			message = message.Replace("legs", "%D");
+			message = message.Replace("chest", "%D");
+			message = message.Replace("body", "%D");
+			message = message.Replace("kevlar vest", "%D");
+			message = message.Replace("nothing", "%D");
+			return message;
+		}
+
+		string IdentifyWeapons(string message)
+		{
+			message = message.Replace("Sniper Rifle", "%W");
+			message = message.Replace("M4 Assault Rifle", "%W");
+			message = message.Replace("MK23 Pistol", "%W");
+			return message;
+		}
+
+		string IdentifyPlayers(string message)
+		{
+			foreach (Player player in GetPlugin<PAction>().Players)
+				message = message.Replace(player.Name, "%K");
+
+			message = message.Replace("nobody", "%K");
+
+			while (message.Contains("%K, %K"))
+				message = message.Replace("%K, %K", "%K");
+
+			while (message.Contains("%K and %K"))
+				message = message.Replace("%K and %K", "%K");
+
+			return message;
+		}
+
 		void LoadPlayerBinds(string nick)
 		{
-			if (File.Exists(Quake.Directory + "Action/Binds/" + nick + ".txt"))
+			string playerBindsPath = Quake.Directory + "Action/Binds/" + nick.NormalizePath() + ".txt";
+			if (File.Exists(playerBindsPath))
 			{
 				Dictionary<string, int> playerBinds = new Dictionary<string, int>();
 
-				using (StreamReader file = new StreamReader(Quake.Directory + "Action/Binds/" + nick + ".txt"))
+				using (StreamReader file = new StreamReader(playerBindsPath))
 				{
 					while (!file.EndOfStream)
 					{
@@ -71,13 +104,12 @@ namespace q2Tool
 
 		void SavePlayersBinds()
 		{
-			foreach (string player in _binds.Keys)
+			foreach (string name in _binds.Keys)
 			{
-				string filePath = Quake.Directory + "Action/Binds/" + player.Replace("/", "") + ".txt";
-				filePath = filePath.Replace("*", "");
+				string filePath = Quake.Directory + "Action/Binds/" + name.NormalizePath() + ".txt";
 				using (StreamWriter file = new StreamWriter(filePath))
 				{
-					var playerBinds = from bind in _binds[player]
+					var playerBinds = from bind in _binds[name]
 									  orderby bind.Value descending
 									  select bind;
 					
